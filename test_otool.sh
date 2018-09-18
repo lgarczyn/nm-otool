246b2ed8aa29f98bcf76cfad88ef24954c108b5b
lgarczyn <lgarczyn@student.42.fr>
2018-09-18 08:59:24 +0200

    fixed test file

--- test_otool.sh
+++ test_otool.sh
@@ -3,7 +3,7 @@ for f in /usr/lib/* /usr/bin/* obj/*
 do
     echo $f
     echo $f >> otool_diff ;
-    otool -dh $f  > /tmp/otool_a
+    otool -tdh $f  > /tmp/otool_a
     ./ft_otool -dh $f > /tmp/otool_b
     diff /tmp/otool_a /tmp/otool_b >> otool_diff
 done
